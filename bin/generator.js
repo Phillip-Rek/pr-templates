160a150603fd312740f99a60f7bd5a737da2c0f1
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-07 19:37:48 +0200

    format: use raise method for ass error reporting

--- bin/generator.js
+++ bin/generator.js
@@ -89,7 +89,7 @@ var Generator = /** @class */ (function () {
                 _this.output += "\n}\n";
             }
             catch (error) {
-                _this.errors.push("[ " + _this.filePath + " ] You have syntax error at line: " + node.line + ", file: " + _this.filePath +
+                _this.raise("[ " + _this.filePath + " ] You have syntax error at line: " + node.line + ", file: " + _this.filePath +
                     (", src: {% " + predicate + " %}\n\nTip: Make sure that you write valid JavaScript code\n\n"));
             }
         };
@@ -107,8 +107,8 @@ var Generator = /** @class */ (function () {
                 new Function("template", "data", testCode)("", _this.data);
             }
             catch (error) {
-                _this.errors.push(error);
-                // this.errors.push(`[ ${this.filePath} ] You have syntax error at line: ${node.line}, file: ${this.filePath}` +
+                _this.raise(error);
+                // this.raise(`[ ${this.filePath} ] You have syntax error at line: ${node.line}, file: ${this.filePath}` +
                 //     `, src: {% ${predicate} %}\n\nTip: Make sure that you write valid JavaScript code\n\n`)
             }
             _this.output += "\n" + predicate + "{\n";
--- src/generator.ts
+++ src/generator.ts
@@ -137,7 +137,7 @@ export class Generator {
 
             this.output += "\n}\n"
         } catch (error) {
-            this.errors.push(`[ ${this.filePath} ] You have syntax error at line: ${node.line}, file: ${this.filePath}` +
+            this.raise(`[ ${this.filePath} ] You have syntax error at line: ${node.line}, file: ${this.filePath}` +
                 `, src: {% ${predicate} %}\n\nTip: Make sure that you write valid JavaScript code\n\n`)
         }
 
@@ -164,8 +164,8 @@ export class Generator {
             new Function("template", "data", testCode)("", this.data)
 
         } catch (error) {
-            this.errors.push(error)
-            // this.errors.push(`[ ${this.filePath} ] You have syntax error at line: ${node.line}, file: ${this.filePath}` +
+            this.raise(error);
+            // this.raise(`[ ${this.filePath} ] You have syntax error at line: ${node.line}, file: ${this.filePath}` +
             //     `, src: {% ${predicate} %}\n\nTip: Make sure that you write valid JavaScript code\n\n`)
         }
 
@@ -180,7 +180,7 @@ export class Generator {
     }
 
     private raise(msg: string) {
-        this.errors.push(msg)
+        this.errors.push(msg);
     }
 }
 
