27a4ab526f8f8ad80abc0d8fd6623ce2a74f5cf9
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-14 15:03:06 +0200

    fix: include source code in if statement error message

--- bin/generator.js
+++ bin/generator.js
@@ -108,7 +108,7 @@ var Generator = /** @class */ (function () {
                 new Function("template", "data", testCode)("", _this.data);
             }
             catch (error) {
-                _this.raise(error + (", line: " + node.line + ", file: " + _this.filePath));
+                _this.raise(error + (", line: " + node.line + ", file: " + _this.filePath + ", src: {% " + predicate + " %}"));
                 // this.raise(`[ ${this.filePath} ] You have syntax error at line: ${node.line}, file: ${this.filePath}` +
                 //     `, src: {% ${predicate} %}\n\nTip: Make sure that you write valid JavaScript code\n\n`)
             }
